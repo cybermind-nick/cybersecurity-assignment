b3d8cf236fc1957e7992f31dc0ac170e21ac4a9a
Almustapha Sulaiman <80785773+SliimmAA@users.noreply.github.com>
2024-02-18 21:20:48 +0000

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,5 +1,24 @@
+import javax.crypto.*;
+import java.security.*;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+
 public class Client {
-    public static void main(String[] args) {
-        
+  public static void main(String[] args) {
+    
+      String host = args[0];
+      int port = Integer.parseInt(args[1]);
+      String userId = args[2];
+    
+    if (args.length != 3) {
+        System.out.println("Please input 3 arguments: host, port, and user id");
+        return;
     }
+
+    // Load the client's private key and the server's public key
+    
+  }
 }
